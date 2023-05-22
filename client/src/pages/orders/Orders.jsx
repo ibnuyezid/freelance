940922700ff24a9bf5c48ccee0a080bc1c0b9e7f
ibnuyezid <tewdudai@gmail.com>
2023-05-22 11:45:54 +0300

    frontend design completed

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,7 +1,105 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./orders.scss";
 function Orders() {
-  return <div>Orders</div>;
+  const currentUser = {
+    id: 1,
+    username: "Jhon doe",
+    isSeller: true,
+  };
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th> Price</th>
+            <th> {currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th> Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88 </td>
+            <td>123</td>
+            <td>
+              <img src="./img/message.png" className="delete" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88 </td>
+            <td>123</td>
+            <td>
+              <img src="./img/message.png" className="delete" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88 </td>
+            <td>123</td>
+            <td>
+              <img src="./img/message.png" className="delete" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88 </td>
+            <td>123</td>
+            <td>
+              <img src="./img/message.png" className="delete" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88 </td>
+            <td>123</td>
+            <td>
+              <img src="./img/message.png" className="delete" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
